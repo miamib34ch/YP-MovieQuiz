8b1beac85602e44634b5fded842727c6136288ed
miami <bapolygalov@edu.hse.ru>
2022-10-29 14:35:09 +0500

    отрефакторил код и убрал возможность нажимать кнопки при загрузке данных

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,11 +1,18 @@
 import UIKit
 
 final class MovieQuizViewController: UIViewController {
-
+    
+    // делает статус бар светлым (у нас всегда темный фон и поэтому внезависимости от темы афйона статус бар будет светлым)
+    override var preferredStatusBarStyle: UIStatusBarStyle { .lightContent }
+    
     @IBOutlet private weak var textLabel: UILabel!
     @IBOutlet private weak var imageView: UIImageView!
     @IBOutlet private weak var counterLabel: UILabel!
+    @IBOutlet weak var yesButton: UIButton!
+    @IBOutlet weak var noButton: UIButton!
     
+    private var currentQuestionIndex: Int = 0
+    private var correctAnswers: Int = 0
     private let questions: [QuizQuestion] = [
         QuizQuestion(
             image: "The Godfather",
@@ -48,50 +55,57 @@ final class MovieQuizViewController: UIViewController {
             text: "Рейтинг этого фильма больше чем 6?",
             correctAnswer: false)
     ]
-    private var currentQuestionIndex: Int = 0
-    private var correctAnswers: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // настройка внешнего вида
+        imageView.layer.masksToBounds = true // включаем маску, которая изменяется для создания эффектов границ и углов
+        imageView.layer.cornerRadius = 20 // радиус скругления углов рамки
+        
+        // показываем первый вопрос
         show(quiz: convert(model: questions[currentQuestionIndex]))
     }
     
-    //делает статус бар айфона светлым (у нас всегда темный фон и поэтому внезависимости от темы афйона статус бар будет светлым)
-    override var preferredStatusBarStyle: UIStatusBarStyle { .lightContent }
-    
-    @IBAction private func yesButtonClicked(_ sender: UIButton) {
+    @IBAction private func yesButtonClicked() {
         let currentQuestion = questions[currentQuestionIndex]
         let givenAnswer = true
         showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
     
-    @IBAction private func noButtonClicked(_ sender: Any) {
+    @IBAction private func noButtonClicked() {
         let currentQuestion = questions[currentQuestionIndex]
         let givenAnswer = false
         showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
     
     private func showAnswerResult(isCorrect: Bool) {
+        //подсчёт очков
         if isCorrect {
-                correctAnswers += 1
-            }
+            correctAnswers += 1
+        }
         
-        imageView.layer.masksToBounds = true
+        // ограничиваем работу кнопок на время загрузки данных
+        yesButton.isEnabled = false
+        noButton.isEnabled = false
+        
+        // показываем рамку правильного или неправильного ответов
         imageView.layer.borderWidth = 8
         imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
-        imageView.layer.cornerRadius = 20 // радиус скругления углов рамки
-            
+        
+        // через секунду показываем следующий вопрос и разрешаем работу кнопок
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
             self.showNextQuestionOrResults()
+            self.yesButton.isEnabled = true
+            self.noButton.isEnabled = true
         }
     }
     
     private func showNextQuestionOrResults() {
         if currentQuestionIndex == questions.count - 1 {
-            let text = "Ваш результат: \(correctAnswers) из 10"
             let viewModel = QuizResultsViewModel(
                 title: "Этот раунд окончен!",
-                text: text,
+                text: "Ваш результат: \(correctAnswers) из 10",
                 buttonText: "Сыграть ещё раз")
             show(quiz: viewModel)
         } else {
@@ -104,15 +118,13 @@ final class MovieQuizViewController: UIViewController {
     }
     
     private func show(quiz step: QuizStepViewModel) {
-        imageView.layer.masksToBounds = true
         imageView.layer.borderWidth = 0
-        imageView.layer.cornerRadius = 20 // радиус скругления углов рамки
         
         textLabel.text = step.question
         counterLabel.text = step.questionNumber
         imageView.image = step.image
     }
-
+    
     private func show(quiz result: QuizResultsViewModel) {
         let alert = UIAlertController(
             title: result.title,
@@ -121,9 +133,7 @@ final class MovieQuizViewController: UIViewController {
         
         let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
             self.currentQuestionIndex = 0
-            
-            // скидываем счётчик правильных ответов
-                self.correctAnswers = 0
+            self.correctAnswers = 0
             
             // заново показываем первый вопрос
             let firstQuestion = self.questions[self.currentQuestionIndex]
@@ -146,23 +156,23 @@ final class MovieQuizViewController: UIViewController {
 
 // для состояния "Вопрос задан"
 private struct QuizStepViewModel {
-  let image: UIImage
-  let question: String
-  let questionNumber: String
+    let image: UIImage
+    let question: String
+    let questionNumber: String
 }
 
 // для состояния "Результат квиза"
 private struct QuizResultsViewModel {
-  let title: String
-  let text: String
-  let buttonText: String
+    let title: String
+    let text: String
+    let buttonText: String
 }
 
 //Структура для хранения Mock-данных
 private struct QuizQuestion {
-  let image: String
-  let text: String
-  let correctAnswer: Bool
+    let image: String
+    let text: String
+    let correctAnswer: Bool
 }
 
 /*
@@ -173,56 +183,56 @@ private struct QuizQuestion {
  Настоящий рейтинг: 9,2
  Вопрос: Рейтинг этого фильма больше чем 6?
  Ответ: ДА
-
-
+ 
+ 
  Картинка: The Dark Knight
  Настоящий рейтинг: 9
  Вопрос: Рейтинг этого фильма больше чем 6?
  Ответ: ДА
-
-
+ 
+ 
  Картинка: Kill Bill
  Настоящий рейтинг: 8,1
  Вопрос: Рейтинг этого фильма больше чем 6?
  Ответ: ДА
-
-
+ 
+ 
  Картинка: The Avengers
  Настоящий рейтинг: 8
  Вопрос: Рейтинг этого фильма больше чем 6?
  Ответ: ДА
-
-
+ 
+ 
  Картинка: Deadpool
  Настоящий рейтинг: 8
  Вопрос: Рейтинг этого фильма больше чем 6?
  Ответ: ДА
-
-
+ 
+ 
  Картинка: The Green Knight
  Настоящий рейтинг: 6,6
  Вопрос: Рейтинг этого фильма больше чем 6?
  Ответ: ДА
-
-
+ 
+ 
  Картинка: Old
  Настоящий рейтинг: 5,8
  Вопрос: Рейтинг этого фильма больше чем 6?
  Ответ: НЕТ
-
-
+ 
+ 
  Картинка: The Ice Age Adventures of Buck Wild
  Настоящий рейтинг: 4,3
  Вопрос: Рейтинг этого фильма больше чем 6?
  Ответ: НЕТ
-
-
+ 
+ 
  Картинка: Tesla
  Настоящий рейтинг: 5,1
  Вопрос: Рейтинг этого фильма больше чем 6?
  Ответ: НЕТ
-
-
+ 
+ 
  Картинка: Vivarium
  Настоящий рейтинг: 5,8
  Вопрос: Рейтинг этого фильма больше чем 6?
