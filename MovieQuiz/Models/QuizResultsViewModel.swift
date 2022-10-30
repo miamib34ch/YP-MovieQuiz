61d81440dc7038d761f6148e1e6488495ec0f18d
miami <bapolygalov@edu.hse.ru>
2022-10-30 20:06:10 +0500

    вынес в отдельные файлы генерацию вопросов и модели

---
+++ MovieQuiz/Models/QuizResultsViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  QuizResultsViewModel.swift
+//  MovieQuiz
+//
+//  Created by Богдан Полыгалов on 30.10.2022.
+//
+
+import Foundation
+
+// для состояния "Результат квиза"
+private struct QuizResultsViewModel {
+    let title: String
+    let text: String
+    let buttonText: String
+}
---
+++ MovieQuiz/Models/QuizStepViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  QuizStepViewModel.swift
+//  MovieQuiz
+//
+//  Created by Богдан Полыгалов on 30.10.2022.
+//
+
+import Foundation
+import UIKit
+
+// для состояния "Вопрос задан"
+private struct QuizStepViewModel {
+    let image: UIImage
+    let question: String
+    let questionNumber: String
+}
