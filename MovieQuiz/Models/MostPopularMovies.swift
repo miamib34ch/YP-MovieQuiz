dd018dc23293452cfb9611c152fd6e0995b3ea7f
miamib34ch <bapolygalov@edu.hse.ru>
2022-11-23 22:23:52 +0500

    Добавил преобразование сетевых данных в данные swift

---
+++ MovieQuiz/Models/MostPopularMovies.swift
@@ -0,0 +1,76 @@
+//
+//  MostPopularMovies.swift
+//  MovieQuiz
+//
+//  Created by Богдан Полыгалов on 22.11.2022.
+//
+
+/*
+ Создаём модель, которая соответсвует ответу от API.
+ Ответ API выглядит следующим образом:
+ 
+ {
+ "errorMessage" : "",
+ "items" : [
+ {
+ "crew" : "Dan Trachtenberg (dir.), Amber Midthunder, Dakota Beavers",
+ "fullTitle" : "Prey (2022)",
+ "id" : "tt11866324",
+ "imDbRating" : "7.2",
+ "imDbRatingCount" : "93332",
+ "image" : "https://m.media-amazon.com/images/M/MV5BMDBlMDYxMDktOTUxMS00MjcxLWE2YjQtNjNhMjNmN2Y3ZDA1XkEyXkFqcGdeQXVyMTM1MTE1NDMx._V1_Ratio0.6716_AL_.jpg",
+ "rank" : "1",
+ "rankUpDown" : "+23",
+ "title" : "Prey",
+ "year" : "2022"
+ },
+ {
+ "crew" : "Anthony Russo (dir.), Ryan Gosling, Chris Evans",
+ "fullTitle" : "The Gray Man (2022)",
+ "id" : "tt1649418",
+ "imDbRating" : "6.5",
+ "imDbRatingCount" : "132890",
+ "image" : "https://m.media-amazon.com/images/M/MV5BOWY4MmFiY2QtMzE1YS00NTg1LWIwOTQtYTI4ZGUzNWIxNTVmXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_Ratio0.6716_AL_.jpg",
+ "rank" : "2",
+ "rankUpDown" : "-1",
+ "title" : "The Gray Man",
+ "year" : "2022"
+ },
+ ...
+ ]
+ }
+ */
+
+import Foundation
+
+struct MostPopularMovies: Codable {
+    let errorMessage: String
+    let items: [MostPopularMovie]
+}
+
+struct MostPopularMovie: Codable {
+    //добавили только поля, которые нас интересуют
+    let title: String
+    let rating: String
+    let imageURL: URL
+    
+    var resizedImageURL: URL {
+        // создаем строку из адреса
+        let urlString = imageURL.absoluteString
+        //  обрезаем лишнюю часть и добавляем модификатор желаемого качества
+        let imageUrlString = urlString.components(separatedBy: "._")[0] + "._V0_UX600_.jpg"
+        
+        // пытаемся создать новый адрес, если не получается возвращаем старый
+        guard let newURL = URL(string: imageUrlString) else {
+            return imageURL
+        }
+        
+        return newURL
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+        case title = "fullTitle"
+        case rating = "imDbRating"
+        case imageURL = "image"
+    }
+}
---
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -0,0 +1,59 @@
+//
+//  MoviesLoader.swift
+//  MovieQuiz
+//
+//  Created by Богдан Полыгалов on 22.11.2022.
+//
+
+import Foundation
+
+protocol MoviesLoading {
+    func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void)
+}
+
+struct MoviesLoader: MoviesLoading {
+    // MARK: - NetworkClient
+    private let networkClient = NetworkClient()
+    
+    // MARK: - URL
+    private var mostPopularMoviesUrl: URL {
+        // Если мы не смогли преобразовать строку в URL, то приложение упадёт с ошибкой
+        guard let url = URL(string: "https://imdb-api.com/en/API/Top250Movies/k_vzyuj6z3") else {
+            preconditionFailure("Unable to construct mostPopularMoviesUrl")
+        }
+        return url
+    }
+    
+    // MARK: - Loading movie and Errors
+    func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void) {
+        
+        networkClient.fetch(url: mostPopularMoviesUrl){ result in //функция которая принимает ответ сетевого запроса (ошибка или дата)
+            
+            switch result{
+            case .failure(let error):   //если приходит ошибка, то передаём её дальше
+                handler(.failure(error))
+                
+            case .success(let data):    //приходят данные
+                do {    //пытаемся их декодировать
+                    let mostPopularMovies = try JSONDecoder().decode(MostPopularMovies.self, from: data)
+                    
+                    //декодировали, но данные пустые, тогда ошибка
+                    if mostPopularMovies.items.isEmpty{
+                        handler(.failure(loadingMovieError.loadError))
+                        return
+                    }
+                    
+                    //всё хорошо, передаём данные
+                    handler(.success(mostPopularMovies))
+                    
+                } catch {//если не получилось, то передаём дальше ошибку ошибку
+                    handler(.failure(loadingMovieError.loadError))
+                }
+            }
+        }
+    }
+    
+    private enum loadingMovieError: Error {
+        case loadError
+    }
+}
